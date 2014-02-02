411b7b43b408cbc15a5543bb54fd72855bb0f5d2
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-02-03 00:14:17 +0100

    alakazam_spec: refactoring

--- lib/alakazam/version.rb
+++ lib/alakazam/version.rb
@@ -9,5 +9,5 @@
 #++
 
 module Alakazam
-  VERSION = '0.3'
+  VERSION = '0.4'
 end
--- tests/alakazam_spec.rb
+++ tests/alakazam_spec.rb
@@ -31,35 +31,40 @@ class Shiftry
 end
 
 class Logger
+  def initialize
+    @i = 0
+  end
+
   def update(*things)
-    $i += 1
+    @i += 1
+  end
+
+  def counter
+    @i
   end
 
   def self.on_fire(*things)
-    $i += 1
+    @@i  = 0 if not defined? @@i
+    @@i += 1
+  end
+
+  def self.counter
+    @@i
   end
 end
 
 describe Alakazam do
   before do
-    $i = 0
-  end
-
-  let(:counter) do
-    counter = $i
+    @counter = 0
   end
 
   let(:logger) do
-    logger = ->(*things) { $i += 1 }
+    ->(*things) {
+      @counter += 1
+    }
   end
 
-  let(:shiftry) do
-    shiftry = Shiftry.new
-  end
-
-  after do
-    $i = 0
-  end
+  let(:shiftry) { Shiftry.new }
 
   it 'invokes a Proc when notified by observed class' do
     shiftry.is_observed_by logger
@@ -70,7 +75,7 @@ describe Alakazam do
     shiftry.lel
     shiftry.lel
 
-    counter.should be 2
+    @counter.should be 2
   end
 
   it 'invokes a Proc without explicit notify when notified by observed class' do
@@ -82,7 +87,7 @@ describe Alakazam do
     shiftry.lel
     shiftry.lel
 
-    counter.should be 4
+    @counter.should be 4
   end
 
   it 'invokes a Proc when notified by a class method of the observed class' do
@@ -91,27 +96,30 @@ describe Alakazam do
     Shiftry.lul
     Shiftry.lul
 
-    counter.should be 2
+    @counter.should be 2
   end
 
   it 'invokes the logger\'s custom and default method when notified by observed class' do
+    logger = Logger.new
+
     shiftry.is_observed_by Logger.new, methods: [ :on_fire, :non_existing_method ]
-    shiftry.is_observed_by Logger.new
+    shiftry.is_observed_by logger
 
     shiftry.lol
 
-    counter.should be 2
+    logger.counter.should be 1
+    Logger.counter.should be 1
   end
 
   it 'invokes the logger\'s default method when a variable changes in the observed class' do
-    shiftry.is_observed_by Logger.new, attributes: { var: :lal, notify: 'fired' }
+    Logger.new.tap { |logger|
+      shiftry.is_observed_by logger, attributes: { var: :lal, notify: 'fired' }
 
-    shiftry.lal = 3
-    shiftry.lal.should be 3
+      shiftry.lal = 3
+      shiftry.lal.should be 3
 
-    shiftry.lal = 4
-    shiftry.lal.should be 4
-
-    counter.should be 2
+      shiftry.lal = 4
+      shiftry.lal.should be 4
+    }.counter.should be 2
   end
 end
